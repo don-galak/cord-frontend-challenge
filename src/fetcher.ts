30ede9400ceec4f0cd607d8fd9089a58cb6946e4
Doruk <ptagalac@gmail.com>
2022-02-13 20:29:59 +0200

    update fetcher file with api endpoints and functions

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,4 +1,38 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from "axios";
 
 // All of your API requests should be in this file
-const apiKey = '1047dd228087fc6ef79efb15641ffd73';
+const apiKey = "1047dd228087fc6ef79efb15641ffd73";
+
+const baseUrl = "https://api.themoviedb.org/3";
+
+const fetcher: AxiosInstance = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const imagePath = 'https://image.tmdb.org/t/p/w200'
+const videosPath = (movieId: string) => `movie/${movieId}/videos`;
+const reviewsPath = (movieId: string) => `movie/${movieId}/reviews`;
+const similarPath = (movieId: string) => `movie/${movieId}/similar`;
+const popularPath = `/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&`;
+const genrePath = `/genre/movie/list?api_key=${apiKey}&language=en-US`;
+
+export const getPopular = async () => {
+  try {
+    const res = await fetcher.get(popularPath);
+    return res.data;
+  } catch (err) {
+    throw new Error(`There was an error fetching the popular movies: ${err}`);
+  }
+};
+
+export const getMovieGenres = async () => {
+  try {
+    const res = await fetcher.get(genrePath);
+    return res.data;
+  } catch (err) {
+    throw new Error(`There was an error fetching the movie genres: ${err}`);
+  }
+};
