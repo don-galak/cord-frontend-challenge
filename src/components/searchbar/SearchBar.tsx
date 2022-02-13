33cd145a3fb8b37966b4856648254b76bb1b9720
Doruk <ptagalac@gmail.com>
2022-02-13 22:48:02 +0200

    update Searchbar with memo and debounce logic

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
+import styled, { css } from "styled-components";
+import debounce from "lodash/debounce";
 
 import * as colors from "../../colors";
 import SearchIcon from "../../images/search-icon-yellow.png";
 import CalendarIcon from "../../images/year-icon.png";
+import { SearchBarProps } from "../../models/components/SearchBar";
 
-export default function SearchBar() {
-  return <div>SEARCH BAR</div>
-}
+const SearchBar = memo(({  onChange }: SearchBarProps) => {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const debouncedChangeHandler = useMemo(
+    () => debounce(changeHandler, 400),
+    []
+  );
+
+  useEffect(() => debouncedChangeHandler.cancel(), []);
+
+  return <Input onChange={debouncedChangeHandler} />;
+});
+
+export default SearchBar;
+
+const Input = styled.input`
+  right: 35px;
+  top: 40%;
+  width: 90%;
+  height: 30px;
+  padding: 10px;
+  outline: 0;
+  border-width: 0 0 2px;
+  border-color: ${colors.primaryColor};
+`;
